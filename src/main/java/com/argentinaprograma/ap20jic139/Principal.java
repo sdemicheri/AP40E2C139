175d483fb7468be7a78eaf7a31cd66d78e4647a8
soledemi <soledemi@yahoo.com.ar>
2023-11-09 19:43:20 -0300

    codigo inicial

---
+++ src/main/java/com/argentinaprograma/ap20jic139/Principal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.argentinaprograma.ap20jic139;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Principal {
+    
+    public static void main(String[] args) {
+        Alumno alumno = new Alumno();
+        alumno.
+    }
+    
+}
